a7cfcfb6f6fb1296222150dda623117471826261
deivydsantoss <deivydl.santoss@gmail.com>
2024-05-25 18:04:12 -0300

    Estilização(incompleta) - Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,6 @@ module.exports = {
 content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        'light-bluee': '1A90DC',
-        'dark-bluee': '04588C',
-        'whitee':'f2f2f2'
-      }
     },
   },
   plugins: [],
