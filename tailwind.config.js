ba28065e843f0375d1a0cce411559fd98d058ce1
deivydsantoss <deivydl.santoss@gmail.com>
2024-05-25 14:24:53 -0300

    Create VirtuaLib
    Fase inicial do site

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'light-bluee': '1A90DC',
+        'dark-bluee': '04588C',
+        'whitee':'f2f2f2'
+      }
+    },
+  },
+  plugins: [],
+}
+
